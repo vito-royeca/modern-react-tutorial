ce800b797313f1975387981aa1ca6290a6037d88
Vito Royeca <vito.royeca@gmail.com>
2026-01-29 02:41:02 -0500

    Redesigned the home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,50 @@
+import { Link } from "react-router";
+
+const Grid = ({ 
+    title,
+    path,
+    icon,
+    text1,
+    text2,
+    isIconOnLeft
+}: {
+    title: string;
+    path: string;
+    icon: string;
+    text1: string;
+    text2: string;
+    isIconOnLeft: boolean;
+}) => {
+    return (
+        <div className={`w3-row-padding ${isIconOnLeft ? 'w3-light-grey' : ''} w3-padding-64 w3-container`}>
+            <div className="w3-content">
+                {isIconOnLeft && (
+                    <div className="w3-third w3-center">
+                        <i className={`fa ${icon} w3-padding-64 w3-text-red w3-margin-right`}></i>
+                    </div>
+                )}
+                <div className="w3-twothird">
+                    <h1>
+                        <Link 
+                            to={path}
+                            className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">
+                            {title}
+                        </Link>
+                    </h1>
+                    <h5 className="w3-padding-32">{text1}</h5>
+                    <p className="w3-text-grey">{text2}</p>
+                </div>
+
+                {!isIconOnLeft && (
+                    <div className="w3-third w3-center">
+                        <i className={`fa ${icon} w3-padding-64 w3-text-red`}></i>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
 const HomePage = () => {
     return ( 
         <>
@@ -5,14 +52,50 @@ const HomePage = () => {
                 <h1 className="w3-margin w3-jumbo">Modern React From the Beginning</h1>
                 <p className="w3-xlarge">Class Projects</p>
             </header>
-            <div className="flex p-30 m-auto justify-center items-center h-full">
-                <ol>
-                    <li><a href="/rating-ui">Rating UI</a></li>
-                    <li><a href="/notes-app">Notes App</a></li>
-                    <li><a href="/cryptodash">Crypto Dash</a></li>
-                    <li><a href="/shopping-cart-ui">Shopping Cart UI</a></li>
-                </ol>
-            </div>
+
+            <Grid 
+                title="Rating UI"
+                path="/rating-ui"
+                icon="fa-star"
+                text1="Simple star rating UI to learn the basics with. Events, props, state, etc."
+                text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat."
+                isIconOnLeft={false}
+            />
+
+            <Grid 
+                title="Notes App"
+                path="/notes-app"
+                icon="fa-book"
+                text1="Create and delete notes from local storage. Learn about forms and state."
+                text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat."
+                isIconOnLeft={true}
+            />
+
+            <Grid 
+                title="Crypto Dash"
+                path="/cryptodash"
+                icon="fa-bitcoin"
+                text1="Use the CoinGecko API to create a dashboard for getting prices and other info from Crypto coins."
+                text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat."
+                isIconOnLeft={false}
+            />
+
+            <Grid   
+                title="Shopping Cart UI"
+                path="/shopping-cart-ui"
+                icon="fa-shopping-cart"
+                text1="Use Context API to build a Shopping Cart application"
+                text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat."
+                isIconOnLeft={true}
+            />
         </>
     );
 }
