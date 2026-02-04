c35cace452487c914b1893da5b9a17e69b99bff7
Vito Royeca <vito.royeca@gmail.com>
2026-02-04 00:23:59 -0500

    Fixed the PDF URL

--- app/routes/pages/about/index.tsx
+++ app/routes/pages/about/index.tsx
@@ -22,7 +22,7 @@ const AboutPage = () => {
             </p>
             <p className="text-gray-500 p-2">
                 <Link 
-                    to='/public/UC-e61ed28d-2af8-46de-ba76-c04745109b09.pdf'
+                    to='/UC-e61ed28d-2af8-46de-ba76-c04745109b09.pdf'
                     target='_blank'>
                     
                     <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
